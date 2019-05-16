264ecbc22460c1df7038067eb38899938d6a8976
Kristaps <Hetachi@users.noreply.github.com>
2019-05-16 11:38:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var port = process.env.PORT || 8081;
 var app = require('express')();
 app.use(function(req, res, next) {
       res.header("Access-Control-Allow-Credentials", "true")
-      res.header("Access-Control-Allow-Origin", "http://exile.lv/feeder");
+      res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       next();
     });
